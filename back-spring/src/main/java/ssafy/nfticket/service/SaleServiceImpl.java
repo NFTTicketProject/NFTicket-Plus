c2975be9c103f28709f1e4609dbb6cc5dea41713
Yun-Gi Oh <orobot13@gmail.com>
2022-06-16 18:18:41 +0900

    feat : implement sale controller's GET method

--- back-spring/src/main/java/ssafy/nfticket/service/SaleServiceImpl.java
+++ back-spring/src/main/java/ssafy/nfticket/service/SaleServiceImpl.java
@@ -3,9 +3,13 @@ package ssafy.nfticket.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import ssafy.nfticket.dto.sale.SaleDto;
 import ssafy.nfticket.entity.Sale;
 import ssafy.nfticket.repository.SaleRepository;
 import ssafy.nfticket.request.SaleRequestDto;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @Transactional(readOnly = true)
@@ -22,8 +26,30 @@ public class SaleServiceImpl implements SaleService {
                 .startedAt(saleRequestDto.getStartedAt())
                 .endedAt(saleRequestDto.getEndedAt())
                 .build();
-        saleRepository.save(sale);
+        Sale saleObj = saleRepository.save(sale);
+        saleRepository.flush();
 
-        return 0L;
+        return saleObj.getId();
+
     }
+
+    @Override
+    @Transactional
+    public List<SaleDto> getSaleList() {
+
+        List<Sale> saleList = saleRepository.findAll();
+        List<SaleDto> saleDtoList = new ArrayList<>();
+        for(Sale sale : saleList){
+            SaleDto saleDto = new SaleDto();
+            saleDto.setShowScheduleId(sale.getShowScheduleId());
+            saleDto.setDescription(sale.getDescription());
+            saleDto.setStartedAt(sale.getStartedAt());
+            saleDto.setEndedAt(sale.getEndedAt());
+            saleDtoList.add(saleDto);
+        }
+
+        return saleDtoList;
+
+    }
+
 }
