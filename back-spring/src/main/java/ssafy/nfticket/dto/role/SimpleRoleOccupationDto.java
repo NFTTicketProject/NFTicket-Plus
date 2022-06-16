6608126baf9c018bea3552087191ebb472615f13
sunminky <suns1502@gmail.com>
2022-06-16 17:51:01 +0900

    Feature:back: Role SimpleRoleOccupationDto

---
+++ back-spring/src/main/java/ssafy/nfticket/dto/role/SimpleRoleOccupationDto.java
@@ -0,0 +1,10 @@
+package ssafy.nfticket.dto.role;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class SimpleRoleOccupationDto {
+    private String occupation;
+}
