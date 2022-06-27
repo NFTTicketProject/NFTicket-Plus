1daebefbc18f4671fe9dad54ff9ef0602cd94846
amlwq <baehaeun1105@gmail.com>
2022-06-27 20:22:46 +0900

    Feature:back: Ticket 관련 기능 구현 1. Ticket Repository 생성 2. /block Api 구현 3. Block Service에서 BlockHash가 아닌 Ticket으로 작업

--- back-spring/src/main/java/ssafy/nfticket/service/BlockService.java
+++ back-spring/src/main/java/ssafy/nfticket/service/BlockService.java
@@ -5,31 +5,30 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ssafy.nfticket.common.error.CustomException;
 import ssafy.nfticket.common.error.ErrorCode;
-import ssafy.nfticket.dto.block.SimpleBlockHashDto;
-import ssafy.nfticket.dto.block.SimpleBlockHashTicketDto;
-import ssafy.nfticket.entity.BlockHash;
-import ssafy.nfticket.repository.BlockHashRepository;
+import ssafy.nfticket.dto.request.block.SimpleBlockHashTicketDto;
+import ssafy.nfticket.entity.Ticket;
+import ssafy.nfticket.repository.TicketRepository;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class BlockService {
 
-    private final BlockHashRepository blockHashRepository;
+    private final TicketRepository ticketRepository;
 
     @Transactional
     public String addBlockHash(SimpleBlockHashTicketDto simpleBlockHashTicketDto) {
-        BlockHash newBlockHash = new BlockHash();
-        newBlockHash.setTicketId(simpleBlockHashTicketDto.getTicketId());
-        newBlockHash.setBlockHash(simpleBlockHashTicketDto.getBlockHash());
-        blockHashRepository.save(newBlockHash);
+        Ticket ticket = ticketRepository.findById(simpleBlockHashTicketDto.getTicketId()).orElseThrow(() ->
+                new CustomException(ErrorCode.DATA_NOT_FOUND));
+        ticket.setBlockHash(simpleBlockHashTicketDto.getBlockHash());
+        ticketRepository.save(ticket);
         return "성공";
     }
 
-    public SimpleBlockHashDto getBlockHash(Integer ticketId) {
-        BlockHash foundBlockHash = blockHashRepository.findByTicketId(ticketId).orElseThrow(() ->
+    public String getBlockHash(Long ticketId) {
+        Ticket ticket = ticketRepository.findById(ticketId).orElseThrow(() ->
                 new CustomException(ErrorCode.DATA_NOT_FOUND));
 
-        return new SimpleBlockHashDto(foundBlockHash.getBlockHash());
+        return ticket.getBlockHash();
     }
 }
