e0066e7b3603d440594b73402e52e7c226cd40b4
amlwq <baehaeun1105@gmail.com>
2022-06-27 21:02:47 +0900

    Feature:back: Service impl로 구현

--- back-spring/src/main/java/ssafy/nfticket/service/BlockService.java
+++ back-spring/src/main/java/ssafy/nfticket/service/BlockService.java
@@ -1,34 +1,11 @@
 package ssafy.nfticket.service;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-import ssafy.nfticket.common.error.CustomException;
-import ssafy.nfticket.common.error.ErrorCode;
 import ssafy.nfticket.dto.request.block.SimpleBlockHashTicketDto;
-import ssafy.nfticket.entity.Ticket;
-import ssafy.nfticket.repository.TicketRepository;
 
 @Service
-@RequiredArgsConstructor
-@Transactional(readOnly = true)
-public class BlockService {
+public interface BlockService {
+    String addBlockHash(SimpleBlockHashTicketDto simpleBlockHashTicketDto);
 
-    private final TicketRepository ticketRepository;
-
-    @Transactional
-    public String addBlockHash(SimpleBlockHashTicketDto simpleBlockHashTicketDto) {
-        Ticket ticket = ticketRepository.findById(simpleBlockHashTicketDto.getTicketId()).orElseThrow(() ->
-                new CustomException(ErrorCode.DATA_NOT_FOUND));
-        ticket.setBlockHash(simpleBlockHashTicketDto.getBlockHash());
-        ticketRepository.save(ticket);
-        return "성공";
-    }
-
-    public String getBlockHash(Long ticketId) {
-        Ticket ticket = ticketRepository.findById(ticketId).orElseThrow(() ->
-                new CustomException(ErrorCode.DATA_NOT_FOUND));
-
-        return ticket.getBlockHash();
-    }
+    String getBlockHash(Long ticketId);
 }
